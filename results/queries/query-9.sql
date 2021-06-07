44355317bfd97e5680fd1809b7a7c7ceaebcdb4a
gabriellamarins <81614041+gabriellamarins@users.noreply.github.com>
2021-06-07 16:19:35 +0200

    query-9

--- results/queries/query-9.sql
+++ results/queries/query-9.sql
@@ -5,4 +5,5 @@ FROM orders
          LEFT JOIN order_product ON orders.id = order_product.order_id
 
          LEFT JOIN products ON order_product.product_id = products.id
+WHERE customer_id = 2
 GROUP BY orders.number
